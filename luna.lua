5e9b93db0278364f11923a803201c80b045501e8
gspezi15 <75540750+gspezi15@users.noreply.github.com>
2022-05-28 12:53:46 +0100

    LittleDialogue

--- luna.lua
+++ luna.lua
@@ -1,3 +1,5 @@
 local areaNames = require("areaNames")
 local warpTransition = require ("warpTransition")
 local minHUD = require("minHUD")
+local littleDialogue = require("littleDialogue")
+littleDialogue.defaultStyleName = "ml"
