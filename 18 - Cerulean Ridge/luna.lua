cedfe20ef3989d8729754d252bd2fff5f1a6b9f1
gspezi15 <75540750+gspezi15@users.noreply.github.com>
2022-06-03 14:27:06 +0100

    Cerulean Ridge

---
+++ 18 - Cerulean Ridge/luna.lua
@@ -0,0 +1,29 @@
+local spawnzones = API.load("spawnzones")
+
+local areaNames = require("areanames")
+
+--Change this in your lua file to have automatically appearing messages on section switch for specific sections:
+areaNames.sectionNames = {
+	[0] = "Cerulean Ridge",
+        [1] = "Coral Labyrinth",
+		[2] = "Secret Area",
+        [3] = "Atoll Maze",
+        [4] = "Descent to Darkness",
+        [5] = "???",
+        [6] = "Rainbow Coin Depths",
+        [7] = "",
+        [8] = "",
+        [9] = "",
+        [10] = "",
+        [11] = "",
+        [12] = "",
+        [13] = "",
+        [14] = "",
+        [15] = "",
+        [16] = "",
+        [17] = "",
+        [18] = "",
+        [19] = "",
+        [20] = ""
+}
+
